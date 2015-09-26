177158ca1299ae21cc23fd2acc68bb6ce7a9f753
Irfan Sharif <irfanmms@hotmail.com>
2015-09-26 01:03:49 -0400

    update bash script to point to changed github username
    changed `zacpier` to `zangent`

--- download_from_github.sh
+++ download_from_github.sh
@@ -7,11 +7,11 @@ cd .icons
 command -v git >/dev/null 2>&1 || { hasgit="no"; }
 if [ "$hasgit" = "no" ]; then
 echo Can\'t find git.  Downloading from zip file.
-wget https://github.com/zacpier/GnomeYosemiteIcons/archive/master.zip
+wget https://github.com/zangent/GnomeYosemiteIcons/archive/master.zip
 unzip -o master.zip
 else
 echo Cloning from GitHub repository.
-git clone https://github.com/zacpier/GnomeYosemiteIcons.git GnomeYosemiteIcons-master
+git clone https://github.com/zangent/GnomeYosemiteIcons.git GnomeYosemiteIcons-master
 fi
 cd GnomeYosemiteIcons-master
 rm download_from_github.sh
